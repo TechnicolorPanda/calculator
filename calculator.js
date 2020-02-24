5164623d9a46cc181d48f001b8697ad561f03ccd
Lorri Romesberg <LorriAnn7@att.net>
2020-02-24 13:40:18 -0500

    added printout on calculator display

--- calculator.js
+++ calculator.js
@@ -1,13 +1,58 @@
-let a = prompt("Select number");
-const operator = prompt ("Operator");
-let b = prompt("Select another number");
-a = Number(a);
-b = Number(b);
-console.log (a);
-console.log (typeof a);
-console.log (b);
-console.log (operator);
-console.log(operate (a,b,operator));
+const output = 0;
+let typing = false;
+let firstNum = "";
+let nextNum = "";
+
+display();
+
+function display() {
+    const output = document.createElement("div");
+    output.classList.add("box");
+    output.textContent = "0";
+    container.appendChild(output);
+    document.getElementById("1").addEventListener("click", function(){
+        joinNums("1");
+    });
+    document.getElementById("2").addEventListener("click", function(){
+        joinNums("2");
+    });
+    document.getElementById("3").addEventListener("click", function(){
+        joinNums("3");
+    });
+    document.getElementById("4").addEventListener("click", function(){
+        joinNums("4");
+    });    
+    document.getElementById("5").addEventListener("click", function(){
+        joinNums("5");
+    });
+    document.getElementById("6").addEventListener("click", function(){
+        joinNums("6");
+    });    
+    document.getElementById("7").addEventListener("click", function(){
+        joinNums("7");
+    });
+    document.getElementById("8").addEventListener("click", function(){
+        joinNums("8");
+    });    
+    document.getElementById("9").addEventListener("click", function(){
+        joinNums("9");
+    });
+    document.getElementById("0").addEventListener("click", function(){
+        joinNums("0");
+    });
+
+    function joinNums(n) {
+        nextNum = n;
+    if (typing == false){
+         output.textContent = nextNum;
+         firstNum = nextNum;
+         typing = true;
+    } else {
+         firstNum += nextNum;
+         output.textContent = firstNum;
+     }
+    };
+}
 
 function operate(a,b,operator) {
     switch (operator) {
@@ -25,6 +70,4 @@ function operate(a,b,operator) {
         break;
     }
 }
-
-
 
