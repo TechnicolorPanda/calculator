386f736088c4c109ee358251ca0b8d58861c8e0b
Lorri Romesberg <LorriAnn7@att.net>
2020-02-25 20:23:43 -0500

    added functionality to compute equations with two numbers

--- calculator.js
+++ calculator.js
@@ -1,15 +1,22 @@
-const output = 0;
+const result = 0;
 let typing = false;
 let firstNum = "";
 let nextNum = "";
+let equation = "";
+let operator = "";
 
 display();
 
 function display() {
     const output = document.createElement("div");
-    output.classList.add("box");
-    output.textContent = "0";
+    output.classList.add("box1");
+    output.textContent = "";
     container.appendChild(output);
+    const result = document.createElement("div");
+    result.classList.add("box2");
+    result.textContent = "0";
+    container.appendChild(result);
+
     document.getElementById("1").addEventListener("click", function(){
         joinNums("1");
     });
@@ -40,24 +47,77 @@ function display() {
     document.getElementById("0").addEventListener("click", function(){
         joinNums("0");
     });
-
+    document.getElementById("plus").addEventListener("click", function(){
+        clickOperator("+");
+    });
+    document.getElementById("minus").addEventListener("click", function(){
+        clickOperator("-");
+    });
+    document.getElementById("times").addEventListener("click", function(){
+        clickOperator("*");
+    });
+    document.getElementById("divide").addEventListener("click", function(){
+        clickOperator("/");
+    });
+    document.getElementById("equals").addEventListener("click", function(){
+        returnAnswer(equation);
+    })
+        
     function joinNums(n) {
         nextNum = n;
-    if (typing == false){
-         output.textContent = nextNum;
+         if (typing == false){
+         result.textContent = nextNum;
          firstNum = nextNum;
          typing = true;
+         return firstNum;
     } else {
-         firstNum += nextNum;
-         output.textContent = firstNum;
-     }
+        operate (firstNum, nextNum, n);
+        firstNum += nextNum;
+        result.textContent = firstNum;
+        return firstNum;
+    }
     };
+
+    function clickOperator(x) {
+        equation = `${equation} ${firstNum} ${x}`;
+        output.textContent = equation;
+        firstNum = 0;
+        typing = false;
+        operator = x;
+    }
+
+    function operate(a,b,operator) {
+        switch (operator) {
+            case "+": 
+                return a+b;
+            break;
+            case "-":
+                return a-b;
+            break;
+            case "*":
+                return a*b;
+            break;
+            case "/":
+                return a/b;
+            break;
+        }
+    }
+
+    function returnAnswer(equation) {
+        equation = `${equation} ${firstNum}`;
+        output.textContent = `${equation} =`;
+        let tmp_arr = equation.split(/[*+-/]/);
+        console.log(tmp_arr[0]);
+        console.log(tmp_arr[1]);
+        let answer = operate(tmp_arr[0], tmp_arr[1], operator);
+        result.textContent = answer;
+    }
 }
 
 function operate(a,b,operator) {
     switch (operator) {
         case "+": 
-            return a + b;
+            return a+b;
         break;
         case "-":
             return a-b;
